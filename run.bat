b54ab537558b6af0ddc8c19166c7b2356c1c55ca
IlyaKrn <ilyakornoukhov@gmail.com>
2024-06-23 07:33:37 +0300

    Dockerfile

--- run.bat
+++
@@ -1,8 +0,0 @@
-start gradlew :IdentityService:bootRun
-start gradlew :ResourceProviderService:bootRun
-start gradlew :DatabaseService:bootRun
-start gradlew :DatabaseTokenService:bootRun
-start gradlew :MailService:bootRun
-start gradlew :ImageService:bootRun
-start gradlew :DriveService:bootRun
-
