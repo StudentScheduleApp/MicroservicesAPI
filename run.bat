2abfa742f0a9bc69dfde97510883812bb734231e
IlyaKrn <ilyakornoukhov@gmail.com>
2024-06-23 05:39:41 +0300

    logs + git + gradle

---
+++ run.bat
@@ -0,0 +1,8 @@
+start gradlew :IdentityService:bootRun
+start gradlew :ResourceProviderService:bootRun
+start gradlew :DatabaseService:bootRun
+start gradlew :DatabaseTokenService:bootRun
+start gradlew :MailService:bootRun
+start gradlew :ImageService:bootRun
+start gradlew :DriveService:bootRun
+
